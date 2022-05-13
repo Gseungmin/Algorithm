2907cb96ca7e919d19ff4ef19e6e909efe611407
Gseungmin <87487149+Gseungmin@users.noreply.github.com>
2022-05-14 00:16:24 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 148 ms, Memory: 143496 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -0,0 +1,12 @@
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int,input().split()))
+List = [0]
+for num in arr:
+    if List[-1] < num:
+        List.append(num)
+    else:
+        List[bisect_left(List, num)] = num
+print(N-(len(List)-1))
