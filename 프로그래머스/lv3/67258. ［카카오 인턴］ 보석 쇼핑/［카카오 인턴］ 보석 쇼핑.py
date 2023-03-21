6d9de292df05c897de44fb836f154fd389b002b2
Ji <cswcsm02@gmail.com>
2023-03-21 13:38:42 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 72.66 ms, Memory: 17.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,38 @@
+def solution(gems):
+    gem = dict()
+    cnt = dict()
+    for i in gems:
+        if i not in cnt:
+            cnt[i] = 0
+    i = 0
+    j = 0
+    ans = int(1e9)
+    List = []
+    while j < len(gems):
+        if len(gem) == len(cnt):
+            while cnt[gems[i]] > 1:
+                cnt[gems[i]] -= 1
+                i += 1
+            if j-i < ans:
+                left = i+1
+                right = j
+                ans = j-i
+            gem[gems[j]] = True
+            cnt[gems[j]] += 1
+            j += 1
+        else:
+            gem[gems[j]] = True
+            cnt[gems[j]] += 1
+            j += 1
+        if j == len(gems):
+            while cnt[gems[i]] > 1:
+                cnt[gems[i]] -= 1
+                i += 1
+            if j-i < ans:
+                left = i+1
+                right = j
+                ans = j-i
+    answer = []
+    answer.append(left)
+    answer.append(right)
+    return answer
