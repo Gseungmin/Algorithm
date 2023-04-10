1e580acefa8a49b6283f9fff6724420637f88765
Ji <cswcsm02@gmail.com>
2023-04-10 14:18:38 +0900

    [Platinum V] Title: 주유소, Time: 412 ms, Memory: 216036 KB -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+import heapq
+N, M = map(int,input().split())
+cost = [0] + list(map(int,input().split())) #주유소 리터당 가격
+graph = [[] for i in range(N+1)]
+for i in range(M):
+    a, b, c = map(int,input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+    
+heap = [] #비용, 최소 주유소, 위치
+heapq.heappush(heap, [0,cost[1],1])
+
+INF = int(1e9)
+dist = [[INF]*(2501) for i in range(N+1)]
+true = [[False]*(2501) for i in range(N+1)]
+dist[1][cost[1]] = 0
+
+while heap:
+    c, k, x = heapq.heappop(heap)
+    if true[x][k] == True:
+        continue
+    true[x][k] = True
+    for nx, d in graph[x]:
+        value = min(cost[nx], k)
+        if true[nx][value] == False:
+            if dist[nx][value] > dist[x][k] + d*k: #d는 거리, k는 기름 최솟값
+                dist[nx][value] = dist[x][k] + d*k
+                heapq.heappush(heap, [dist[nx][value], value, nx])
+
+ans = INF
+for i in dist[N]:
+    ans = min(ans, i)
+
+print(ans)
