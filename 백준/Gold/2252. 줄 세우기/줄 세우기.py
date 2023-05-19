75e78e98d709f4b0f36ce9041009c41465361e31
Ji <cswcsm02@gmail.com>
2023-05-19 12:17:53 +0900

    [Gold III] Title: 줄 세우기, Time: 232 ms, Memory: 123648 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
+
 N, M = map(int,input().split())
-graph = [[] for i in range(N+1)]
-ind = [0]*(N+1)
-true = [0]*(N+1)
+
+student = [0]*(N+1)
+info = [[] for i in range(N+1)]
 for i in range(M):
     a, b = map(int,input().split())
-    graph[a].append(b)
-    ind[b] += 1
-
+    info[a].append(b)
+    student[b] += 1
 
 from collections import deque
+true = [False]*(N+1)
 queue = deque()
 for i in range(1,N+1):
-    if ind[i] == 0:
+    if student[i] == 0:
         queue.append(i)
-        true[i] = 1
+        true[i] = True
+
+ans = []
 while queue:
     x = queue.popleft()
-    print(x, end = " ")
-    for dx in graph[x]:
-        if true[dx] == 0:
-            ind[dx] -= 1
-            if ind[dx] == 0:
-                queue.append(dx)
-                true[dx] = 1
+    ans.append(x)
+    for nx in info[x]:
+        if true[nx] == False:
+            student[nx] -= 1
+            if student[nx] == 0:
+                true[nx] = True
+                queue.append(nx)
+print(" ".join(map(str,ans)))
