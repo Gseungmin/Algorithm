8eb55aca15c716823404c4aa7667c7d8ddcce64d
Ji <cswcsm02@gmail.com>
2023-06-24 13:14:24 +0900

    [level 3] Title: 양과 늑대, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,51 +1,40 @@
-#양의 수가 더 많아야 함
-def RC(now, wolf, sheep, true, info, graph,answer):
+#양과 늑대가 놓여 있음
+#양이 더 많아야 함
+#최대한 많은 수의 양을 모아 루트 노드로 돌아와야 함
+#0은 양, 1은 늑대, 0번 노드에서 시작
+
+#sheep: 양의수, wolf: 늑대수, nodes: 현재 선택된 노드들, graph: 노드 그래프
+def RC(sheep, wolf, nodes, graph, ans, info):
     
-    if wolf == sheep:
+    if sheep <= wolf:
         return
     
-    answer[0] = max(answer[0], sheep)
+    ans[0] = max(ans[0], sheep)
     
-    new_now = []
-    for i in now:
-        check = True
-        for j in graph[i]:
-            if true[j] == False:
-                new_now.append(i)
-                break
-        
-    for i in now:
-        for j in graph[i]:
-            if true[j] == False:
-                if info[j] == 0: #양일 경우
-                    true[j] = True
-                    new_now.append(j)
-                    RC(new_now, wolf, sheep+1, true, info, graph,answer)
-                    new_now.pop()
-                    true[j] = False
-                elif info[j] == 1: #늑대일 경우
-                    true[j] = True
-                    new_now.append(j)
-                    RC(new_now, wolf+1, sheep, true, info, graph,answer)
-                    new_now.pop()
-                    true[j] = False
+    for x in nodes:
+        for nx in graph[x]:
+            if nx not in nodes:
+                nodes.add(nx)
+                if info[nx] == 0:
+                    RC(sheep+1, wolf, nodes, graph, ans, info)
+                else:
+                    RC(sheep, wolf+1, nodes, graph, ans, info)
+                nodes.discard(nx)
     
     return
 
+#cond 초기화 및 RC를 통해 문제 해결
 def solution(info, edges):
-    answer = [0]
-    
     graph = [[] for i in range(len(info))]
     
     for i, j in edges:
         graph[i].append(j)
+        graph[j].append(i)
+        
+    nodes = set()
+    nodes.add(0)
     
-    wolf = 0
-    sheep = 1
-    now = [0]
-    true = [False]*len(info)
-    true[0] = True
+    ans = [1]
+    RC(1,0,nodes, graph, ans, info)
     
-    RC(now, wolf, sheep, true, info, graph, answer)
-    
-    return answer[0]
+    return ans[0]
